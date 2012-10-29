69e547e26fed4e3485028ae50de93328b949d5fa
Asko Soukka <asko.soukka@iki.fi>
2012-10-29 16:48:09 +0200

    Fixed to correctly bind schema field to adapted context and get value through the bound field.

--- src/collective/roster/behaviors/indexers.py
+++ src/collective/roster/behaviors/indexers.py
@@ -7,6 +7,8 @@ from plone.indexer import indexer
 @indexer(IHasRelatedPersons)
 def RelatedPersonsIndexer(context):
     """ create index from UUID list so we can search catalog with it"""
-    return IRelatedPersons(context).related_persons
+    adapted = IRelatedPersons(context)
+    bound = IRelatedPersons["related_persons"].bind(adapted)
+    return bound.get(adapted)
 
 grok.global_adapter(RelatedPersonsIndexer, name="related_persons")
