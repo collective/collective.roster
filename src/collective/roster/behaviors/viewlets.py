1b71f29d88362a0b06c129b5629e703adb7609cf
Toni Haka-Risku <toni.haka-risku@jyu.fi>
2013-01-29 14:01:39 +0200

    office info added

--- src/collective/roster/behaviors/viewlets.py
+++ src/collective/roster/behaviors/viewlets.py
@@ -4,13 +4,21 @@ from five import grok
 
 from collective.roster.person import PersonViewlets
 
-from collective.roster.behaviors.interfaces import IContactInfo
+from collective.roster.behaviors.interfaces import (
+    IContactInfo,
+    IOfficeInfo
+)
 
 
 class ContactInfoViewlet(grok.Viewlet):
-    """Viewlet for rendering the contact info.
-    """
-
+    """Viewlet for rendering the contact info."""
     grok.viewletmanager(PersonViewlets)
     grok.context(IContactInfo)
     grok.name("collective.roster.personviewlets.contactinfo")
+
+
+class OfficeInfoViewlet(grok.Viewlet):
+    """Viewlet for rendering the contact info."""
+    grok.viewletmanager(PersonViewlets)
+    grok.context(IOfficeInfo)
+    grok.name("collective.roster.personviewlets.officeinfo")
