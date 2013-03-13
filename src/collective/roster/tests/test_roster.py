0b359ff7973de6c265bd35ddb01ee33ebba199b6
Asko Soukka <asko.soukka@iki.fi>
2013-03-13 09:33:34 +0200

    Add minimal integration tests for roster vocabularies

---
+++ src/collective/roster/tests/test_roster.py
@@ -0,0 +1,44 @@
+#-*- coding: utf-8 -*-
+import unittest2 as unittest
+from collective.roster.testing import ROSTER_INTEGRATION_TESTING
+
+
+class RosterIntegrationTests(unittest.TestCase):
+
+    layer = ROSTER_INTEGRATION_TESTING
+
+    def setUp(self):
+        from plone.dexterity.utils import createContentInContainer
+        portal = self.layer["portal"]
+        data = {
+            "id": "example-roster",
+            "groups": [u"Alfa|Alfa coders", u"Beta|Beta testers"]
+        }
+        createContentInContainer(portal, "collective.roster.roster",
+                                 checkConstraints=False, **data)
+        self.roster = portal["example-roster"]
+
+    def testRosterLocalGroupsVocabulary(self):
+        from collective.roster.roster import LocalGroupsVocabulary
+        vocabulary = LocalGroupsVocabulary()(self.roster)
+
+        self.assertEqual(len(vocabulary), 2)
+
+        to_tuple = lambda term: (term.value, term.token, term.title)
+        tuples = map(to_tuple, vocabulary)
+
+        self.assertIn((u"Alfa", "alfa-alfa-coders", u"Alfa coders"), tuples)
+        self.assertIn((u"Beta", "beta-beta-testers", u"Beta testers"), tuples)
+
+    def testRosterDisplayColumnsVocabulary(self):
+        from collective.roster.roster import DisplayColumnsVocabulary
+        vocabulary = DisplayColumnsVocabulary()(self.roster)
+
+        self.assertNotEqual(len(vocabulary), 0)
+
+        to_tuple = lambda term: (term.value, term.token, term.title)
+        tuples = map(to_tuple, vocabulary)
+
+        self.assertIn(("collective.roster.personnellisting.title",
+                       "collective.roster.personnellisting.title",
+                       u"Name"), tuples)
