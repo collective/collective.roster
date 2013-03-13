13fda4e9dff8c045851fb637ee0f25af32b2ae97
Asko Soukka <asko.soukka@iki.fi>
2013-03-13 07:57:47 +0200

    Refactor test setup to be independent from plone.act

--- src/collective/roster/testing.py
+++ src/collective/roster/testing.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from Acquisition import aq_base
+from Products.CMFPlone.tests.utils import MockMailHost
+from Products.MailHost.interfaces import IMailHost
 from plone.app.testing import (
     PloneSandboxLayer,
     PLONE_FIXTURE,
@@ -6,13 +10,7 @@ from plone.app.testing import (
     applyProfile
 )
 from plone.testing import z2
-
-from Products.MailHost.interfaces import IMailHost
-from Products.CMFPlone.tests.utils import MockMailHost
-from Acquisition import aq_base
-
 from zope.component import getSiteManager
-from OFS.SimpleItem import SimpleItem
 
 
 class Layer(PloneSandboxLayer):
@@ -23,9 +21,6 @@ class Layer(PloneSandboxLayer):
         self.loadZCML(package=collective.roster)
 
     def setUpPloneSite(self, portal):
-        # Set our Robot Framework remote library
-        portal._setObject("RosterRemoteLibrary", RemoteLibrary())
-
         # Replace MailHost with MockMailHost
         portal._original_MailHost = portal.MailHost
         portal.MailHost = mailhost = MockMailHost('MailHost')
@@ -36,51 +31,12 @@ class Layer(PloneSandboxLayer):
         applyProfile(portal, 'collective.roster:default')
 
     def tearDownPloneSite(self, portal):
-        # Remove our Robot Framework remote library
-        portal._delObject("RosterRemoteLibrary")
-
         # Restore the original MailHost
         portal.MailHost = portal._original_MailHost
         sm = getSiteManager(context=portal)
         sm.unregisterUtility(provided=IMailHost)
         sm.registerUtility(aq_base(portal._original_MailHost),
                            provided=IMailHost)
-
-
-class RemoteLibrary(SimpleItem):
-    """Robot Framework Remote Library"""
-
-    def get_keyword_names(self):
-        """ """
-        keywords = Keywords()
-        names = filter(lambda x: x[0] != "_", dir(keywords))
-        return names
-
-    def run_keyword(self, name, args):
-        """ """
-        keywords = Keywords()
-        func = getattr(keywords, name)
-        result = {"error": "", "return": ""}
-        try:
-            retval = func(*args)
-        except Exception, e:
-            result["status"] = "FAIL"
-            result["error"] = str(e)
-        else:
-            result["status"] = "PASS"
-            result["return"] = retval
-        return result
-
-
-class Keywords(object):
-
-    def product_has_been_activated(self, product_name):
-        from zope.component.hooks import getSite
-        from Products.CMFCore.utils import getToolByName
-
-        quickinstaller = getToolByName(getSite(), "portal_quickinstaller")
-
-        assert quickinstaller.isProductInstalled(product_name)
 
 ROSTER_FIXTURE = Layer()
 
@@ -102,7 +58,7 @@ class RobotLayer(PloneSandboxLayer):
         portal._delObject("RemoteKeywordsLibrary")
 
 
-ROBOT_FIXTURE = RobotLayer()
+ROSTER_ROBOT_FIXTURE = RobotLayer()
 
-ROBOT_TESTING = FunctionalTesting(
-    bases=(ROBOT_FIXTURE, z2.ZSERVER_FIXTURE), name="Robot")
+ROSTER_ROBOT_TESTING = FunctionalTesting(
+    bases=(ROSTER_ROBOT_FIXTURE, z2.ZSERVER_FIXTURE), name="Robot")
