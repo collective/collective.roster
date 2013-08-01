03f1db0446179b2b5da49cb7bffa4b22f860b1f9
Tommi Teistela <tommi.t.teistela@jyu.fi>
2013-08-01 14:51:50 +0300

    Allow toggling the groups-as-subjects/keywords behavior via a ... Dexterity behavior
    Enabled by default. Run the included upgrade step to keep behavior consistent on sites already using a roster.

--- src/collective/roster/behaviors/indexers.py
+++ src/collective/roster/behaviors/indexers.py
@@ -7,8 +7,11 @@ from plone.indexer import indexer
 
 from collective.roster.behaviors.interfaces import (
     IHasRelatedPersons,
-    IRelatedPersons
+    IRelatedPersons,
+    IGroupsAsSubjects
 )
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 
 
 @indexer(IHasRelatedPersons)
@@ -19,3 +22,18 @@ def RelatedPersonsIndexer(context):
     return bound.get(adapted)
 
 grok.global_adapter(RelatedPersonsIndexer, name="related_persons")
+
+
+@indexer(IGroupsAsSubjects)
+def subject(context):
+    vocabulary_factory = getUtility(IVocabularyFactory,
+                                    name="collective.roster.localgroups")
+    vocabulary = vocabulary_factory(context)
+    group_terms_filter = lambda term: term.value in context.groups
+
+    terms = filter(group_terms_filter, vocabulary)
+
+    groups = map(lambda term: term.title.encode("utf-8"), terms)
+
+    return context.subject + tuple(groups)
+grok.global_adapter(subject, name="Subject")
