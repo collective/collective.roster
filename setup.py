8aed65d5ceda928107e3cdb3283af87b39461e85
Matti Pohjanvirta <matti.l.pohjanvirta@student.jyu.fi>
2015-12-18 12:25:01 +0200

    Preparing release 1.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="collective.roster",
-    version='1.6.1.dev0',
+    version='1.6.1',
     description="Extendable Personnel Roster",
     long_description=open("README.txt").read() + "\n" +
                 open("CHANGES.txt").read(),
