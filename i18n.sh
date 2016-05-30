2d435a32f7b85d4883729c48a2bb70e683120000
Asko Soukka <asko.soukka@iki.fi>
2016-05-30 14:16:08 +0300

    Add image upload validator for portrait field

--- i18n.sh
+++ i18n.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ./bin/i18ndude rebuild-pot --pot src/collective/roster/locales/collective.roster.pot --merge src/collective/roster/locales/manual.pot --create collective.roster src/collective/roster
 
 ./bin/i18ndude sync --pot src/collective/roster/locales/collective.roster.pot src/collective/roster/locales/*/LC_MESSAGES/collective.roster.po
