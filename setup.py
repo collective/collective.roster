74820d29d0a3f84f4cfc93da4024363d016801ee
Asko Soukka <asko.soukka@iki.fi>
2017-10-27 13:44:44 +0300

    Back to development: 2.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.roster',
-    version='2.3.0',
+    version='2.3.1.dev0',
     description='Personnel Roster',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGELOG.rst').read()),
